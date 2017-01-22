2fbed929dc87b7ed09690f59c983ed03b3ed5d2a
antonio <antonio@biruseto>
2017-01-22 21:49:47 +0100

    Clase Calcularcentigrados

---
+++ ConversorTemperaturas/src/Boletin15/ConversorTemperaturas.java
@@ -0,0 +1,30 @@
+package Boletin15;
+
+public class ConversorTemperaturas {
+
+    private final int TempCelsius = 80;
+
+    public float calcularcentigradosAFharenheit(float temperatura) throws TemperaturaErradaExcepcion {
+        if (temperatura < 80) {
+            throw new TemperaturaErradaExcepcion("temperatura <80ºC");
+        } else 
+            //float Fharenheit = (float)(9/5*temperatura+32,4);
+        
+            return temperatura;
+        
+    }
+}
+
+    
+
+    
+    
+       
+            
+        
+    
+    
+        
+    
+
+
